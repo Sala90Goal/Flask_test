e7d195568dbd85ea30cb07e7299174dd9a718cba
Салават Хабибулин <salavatkhabibulin@MacBook-Air-Salavat.local>
2023-06-22 11:59:24 +0600

    flask test

---
+++ hello.py
@@ -0,0 +1,17 @@
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def hello():
+#     return 'Hello, World!'
+
+# python3 -m venv venv
+# source venv/bin/activate
+# touch hello.py
+# pip install flask
+# python -c "import flask; print(flask.__version__)"
+
+#export FLASK_APP=hello
+# export FLASK_ENV=development
+# flask run
